4c91e68e9ce1cb29d2c00472e1024127102bdeff
Yankhoba <bagayoko.yan@gmail.com>
2024-01-17 11:48:06 +0100

    📝 gérer les affichages

---
+++ src/Form/CommentType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use App\Entity\post;
+use App\Entity\user;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('created_at')
+            ->add('author', EntityType::class, [
+                'class' => user::class,
+'choice_label' => 'id',
+            ])
+            ->add('post', EntityType::class, [
+                'class' => post::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
---
+++ src/Form/PostType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use App\Entity\category;
+use App\Entity\user;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('extract')
+            ->add('content')
+            ->add('image')
+            ->add('isPublished')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('author', EntityType::class, [
+                'class' => user::class,
+'choice_label' => 'id',
+            ])
+            ->add('category', EntityType::class, [
+                'class' => category::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
