a35da0011a1c76ca1bd67f7ad32d4475ce659c44
Yankhoba <bagayoko.yan@gmail.com>
2024-01-17 16:58:47 +0100

    🚧 mise en place des modifications des catégories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,80 +2,86 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
-#[Route('/category')]
 class CategoryController extends AbstractController
 {
-    #[Route('/', name: 'app_category_index', methods: ['GET'])]
-    public function index(CategoryRepository $categoryRepository): Response
+    #[Route('/{category}/edit', name: 'category_edit')]
+    public function edit(
+        Request $request,
+        CategoryRepository $categoryRepository,        
+        EntityManagerInterface $em,
+    ): Response
     {
-        return $this->render('category/index.html.twig', [
-            'categories' => $categoryRepository->findAll(),
+        // Find the category by its name
+        $category = $categoryRepository->findOneBy([
+            'name' => $request->get('category')
         ]);
-    }
 
-    #[Route('/new', name: 'app_category_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $category = new Category();
+        // Edit form + process
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($category);
-            $entityManager->flush();
+        if($form->isSubmitted() && $form->isValid()){
+            if ($form->get('image')->getData()) {
+                $imageFile = $form->get('image')->getData();
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = $originalFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
 
-            return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+                try {
+                    $imageFile->move(
+                        $this->getParameter('uploads_directory'),
+                        $newFilename
+                    );
+                    $category->setImage($newFilename);
+                } catch (FileException $e) {
+                    $this->addFlash('danger', 'Une erreur est survenue lors de l\'upload de votre Image');
+                }
+            }
+
+            $category->setName($form->get('name')->getData());
+            $em->persist($category);
+            $em->flush();
+            return $this->redirectToRoute('category',[
+                'category'=> $category->getName()
+            ]);
         }
+        
 
-        return $this->render('category/new.html.twig', [
-            'category' => $category,
-            'form' => $form,
-        ]);
-    }
 
-    #[Route('/{id}', name: 'app_category_show', methods: ['GET'])]
-    public function show(Category $category): Response
-    {
-        return $this->render('category/show.html.twig', [
-            'category' => $category,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_category_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Category $category, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
-        }
-
+        // Return the view
         return $this->render('category/edit.html.twig', [
+            // Pass the category object to the view
             'category' => $category,
-            'form' => $form,
+            'editForm'=>$form
         ]);
     }
 
-    #[Route('/{id}', name: 'app_category_delete', methods: ['POST'])]
-    public function delete(Request $request, Category $category, EntityManagerInterface $entityManager): Response
+    // Route to add a new category
+    #[Route('/new-category', name: 'category_new')]
+    public function new(
+        // Inject the request object to get the data from the form
+        Request $request,
+        // Add the EntityManagerInterface to save the category
+        EntityManagerInterface $em,
+    ): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($category);
-            $entityManager->flush();
-        }
+        // TODO Add the form here
 
-        return $this->redirectToRoute('app_category_index', [], Response::HTTP_SEE_OTHER);
+        // TODO Add the form proccess here
+
+        // Return the view
+        return $this->render('category/new.html.twig', [
+            // Pass the form to the view
+        ]);
     }
+
+    // Route to delete a category
 }
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,11 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('image')
+            ->add('name', TextType::class)
+            ->add('image', FileType::class, [
+                'mapped' =>false,
+            ] )
+            ->add('Enregistrer', SubmitType::class)
         ;
     }
 
