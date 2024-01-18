60eb16f8f20066df6d1baa214a0dd1eda2a69f8c
Yankhoba <bagayoko.yan@gmail.com>
2024-01-18 12:37:38 +0100

    📝 Modification d'un article

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,7 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,11 +21,54 @@ class PostController extends AbstractController
             'controller_name' => 'PostController',
         ]);
     }
+
     #[Route('/posts/{title}', name: 'show', methods: ['GET'])]
-public function showPost(Post $post): Response
-{
-    return $this->render('post/show.html.twig', [
-        'post' => $post,
-    ]);
-}
+    public function showPost(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/post/{title}/edit', name: 'post_edit')]
+    public function edit(
+        Request $request,
+        EntityManagerInterface $em,
+        Post $post
+    ): Response {
+        // Edit form + process
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('image')->getData()) {
+                $imageFile = $form->get('image')->getData();
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = $originalFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('uploads_directory'),
+                        $newFilename
+                    );
+                    $post->setImage($newFilename);
+                } catch (FileException $e) {
+                    $this->addFlash('danger', 'Une erreur est survenue lors de l\'upload de votre Image');
+                }
+            }
+
+            $post->setTitle($form->get('title')->getData());
+            $em->persist($post);
+            $em->flush();
+
+            // Redirigez l'utilisateur vers la page de détails du post édité
+            return $this->redirectToRoute('show', ['title' => $post->getTitle()]);
+        }
+
+        // Return the view
+        return $this->render('post/edit.html.twig', [
+            'post' => $post,
+            'editForm' => $form->createView(),
+        ]);
+    }
 }
